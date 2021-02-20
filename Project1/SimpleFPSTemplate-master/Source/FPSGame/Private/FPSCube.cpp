4da13104f5ee889a23198fb3d0cd1cd00dd022db
Lansingh Freeman <chicken.fries.are.good@gmail.com>
2021-02-20 13:38:58 -0500

    now with 4 cubes

--- Project1/SimpleFPSTemplate-master/Source/FPSGame/Private/FPSCube.cpp
+++ Project1/SimpleFPSTemplate-master/Source/FPSGame/Private/FPSCube.cpp
@@ -9,10 +9,22 @@ void AFPSCube::Explode()
 	{
 		FVector Scale = this->GetActorScale();
 		Scale *= 0.25f;
-		//AFPSCube::StaticClass(), OtherComp->GetComponentLocation(), OtherComp->GetComponentRotation()
-		AFPSCube* smallCube = GetWorld()->SpawnActor<AFPSCube>(BlueprintCube, GetActorLocation(), GetActorRotation());
-		smallCube->GetRootComponent()->SetWorldScale3D(Scale);
-		smallCube->canExplode = true;
+		
+		float OffScale = 10.0f;
+
+		FVector Offsets[] = {
+			FVector(1,1,0),
+			FVector(1,-1,0),
+			FVector(-1,-1,0),
+			FVector(-1,1,0)
+		};
+
+		for (int x = 0; x < 4; ++x)
+		{
+			AFPSCube* smallCube = GetWorld()->SpawnActor<AFPSCube>(BlueprintCube, GetActorLocation() + (Offsets[x] * OffScale * this->GetActorScale().X), GetActorRotation());
+			smallCube->GetRootComponent()->SetWorldScale3D(Scale);
+			smallCube->canExplode = true;
+		}
 	}
 	else 
 	{
